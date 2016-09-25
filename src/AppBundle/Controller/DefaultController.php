94250e62700fc2923966a13c61e481badbbe6e71
Oliver Hoff <oliver@hofff.com>
2016-09-25 10:57:11 +0200

    double submit cookie demo

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,18 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $builder = $this->createFormBuilder();
+        $builder->add('content', TextType::class);
+        $builder->add('submit', SubmitType::class);
+        $form = $builder->getForm();
+        $form->handleRequest($request);
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'base_dir'  => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'form'      => $form->createView(),
+            'submitted' => $form->isSubmitted(),
+            'valid'     => $form->isValid(),
         ]);
     }
 }
